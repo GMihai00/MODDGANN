ffbca106410d015746aed956b4638ee19e3cb313
Mihai Andrei Gherghinescu <mihai.gherghinescu00@e-uvt.ro>
2023-09-14 21:43:20 +0300

    Started colecting images for dataset

--- example_create_model.py
+++ example_create_model.py
@@ -1,15 +1,6 @@
-#EXAMPLE TAKEN FROM HARDWARD CLASS
-
 import subprocess
 import tensorflow as tf
-
-IMAGE_DIM = (28, 28)
-KERNEL_SIZE = (3, 3)
-FILTERS_NUMBER = 32
-RGB = False
-POOL_SIZE = (2, 2)
-NR_HIDDEN_NODES = 128
-NR_OUTPURS = 10
+import os
 # Use MNIST handwriting dataset
 mnist = tf.keras.datasets.mnist
 
@@ -28,20 +19,20 @@ x_test = x_test.reshape(
 model = tf.keras.models.Sequential([
 
     tf.keras.layers.Conv2D(
-        FILTERS_NUMBER, KERNEL_SIZE, activation="relu", input_shape=(IMAGE_DIM, 3 if RGB else 1)
+        32, (3, 3), activation="relu", input_shape=(28, 28, 1)
     ),
 
-    tf.keras.layers.MaxPooling2D(pool_size=POOL_SIZE),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
     # Flatten units
     tf.keras.layers.Flatten(),
 
     # Add a hidden layer with dropout
-    tf.keras.layers.Dense(NR_HIDDEN_NODES, activation="relu"),
+    tf.keras.layers.Dense(128, activation="relu"),
     tf.keras.layers.Dropout(0.5),
 
     # Add an output layer with output units for all 10 digits
-    tf.keras.layers.Dense(NR_OUTPURS, activation="softmax")
+    tf.keras.layers.Dense(10, activation="softmax")
 ])
 
 # Train neural network
@@ -55,5 +46,7 @@ model.fit(x_train, y_train, epochs=10)
 # Evaluate neural network performance
 model.evaluate(x_test,  y_test, verbose=2)
 
-result = subprocess.run(["mkdir", "-p", "saved_model"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+if not os.path.exists("./saved_model"):
+    subprocess.run(["mkdir", "-p", "saved_model"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+
 model.save('saved_model/my_model')
