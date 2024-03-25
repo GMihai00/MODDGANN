ae6c44ee62f17325ad144eec71a7d773215dacc9
GMihai00 <mihai.gherghinscu15@gmail.com>
2024-03-25 15:54:33 +0200

    comment

--- src/model/create_model.py
+++ src/model/create_model.py
@@ -19,9 +19,18 @@ EXPECTED_PHOTO_WIDTH = 320
 EXPECTED_PHOTO_HEIGHT = 320
 
 IS_RGB = False
-NR_DISEASES = 0
 
 WEIGHTS_BACKUP = 'my_model_weights.h5'
+
+
+DISEASE_TO_CATEGORY = {
+    "pharyngitis": 0,
+    "tonsillitis": 1,
+    "gastric reflux": 2,
+    "tonsil stones": 3,
+    "healthy" : 4
+}
+NR_DISEASES = len(DISEASE_TO_CATEGORY)
 
 def convert_path(path):
     if os.name == 'posix':
@@ -58,14 +67,7 @@ def read_data(file_path):
     
     x_data = []
     y_data = []
-
-    disease_to_category = {
-        "pharyngitis": 0,
-        "tonsillitis": 1,
-        "gastric reflux": 2,
-        "tonsil stones": 3,
-        "healthy" : 4
-    }
+    
     # Open the CSV file for reading
     with open(file_path, mode='r') as file:
         # Create a CSV reader
@@ -82,7 +84,7 @@ def read_data(file_path):
                 
                 if len(image_bytes) != 0:
                     x_data.append(image_bytes)
-                    y_data.append(disease_to_category[disease])
+                    y_data.append(DISEASE_TO_CATEGORY[disease])
                         
 
     return (x_data, y_data)
@@ -149,6 +151,7 @@ def main():
     
     model.summary()
     
+    # problems here to see what is going on rn, maybe input dimensions are just wrong
     model.fit(x_train, y_train, epochs=train_epochs)
     
     # Evaluate neural network performance
