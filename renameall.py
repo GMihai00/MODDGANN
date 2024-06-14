b8572df800ca3d092644eedbd2a92613a6a6a931
GMihai00 <mihai.gherghinscu15@gmail.com>
2024-06-14 17:45:48 +0300

    renaming images

---
+++ renameall.py
@@ -0,0 +1,27 @@
+import os
+
+def rename_files(directory):
+    # List all files in the directory
+    files = os.listdir(directory)
+
+    # Sort files to ensure consistent naming
+    files.sort()
+
+    # Loop through files and rename them
+    for i, filename in enumerate(files):
+        # Generate new file name
+        new_name = f"{i + 1}.jpg"  # Change the extension as needed
+
+        # Full old and new file paths
+        old_path = os.path.join(directory, filename)
+        new_path = os.path.join(directory, new_name)
+
+        # Rename file
+        os.rename(old_path, new_path)
+        print(f"{new_path},tonsillitis")
+
+# Set the directory path
+directory_path = "/home/mgherghinescu/projects/TenserflowModelTraining/bucal_cavity_diseases_dataset/kaggle"
+
+# Call the function
+rename_files(directory_path)
