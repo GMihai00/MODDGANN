3a16a2af4edfc9b0e7b3755173e954279cb815ca
GMihai00 <mihai.gherghinscu15@gmail.com>
2024-06-23 15:30:46 +0300

    important comment

--- src/model/gann/helpers.py
+++ src/model/gann/helpers.py
@@ -2,10 +2,11 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import tensorflow as tf
+import math
 
 from data_processing_helpers import IS_RGB
 
-PREVIOUS_EPOCHS = 0
+PREVIOUS_EPOCHS = 50
 
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
@@ -13,7 +14,8 @@ generator_optimizer = tf.keras.optimizers.Adam(1e-4)
 discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
 noise_dim = 100
-num_examples_to_generate = 1
+# change this to 1 when you want to just generate images for your model
+num_examples_to_generate = 16
 
 # You will reuse this seed overtime (so it's easier)
 # to visualize progress in the animated GIF)
@@ -32,11 +34,14 @@ def generate_and_save_images(model, epoch, test_input):
     # Notice `training` is set to False.
     # This is so all layers run in inference mode (batchnorm).
     predictions = model(test_input, training=False)
-
-    fig = plt.figure(figsize=(1, 1))
-    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+    
+    ratio = int(math.sqrt(num_examples_to_generate))
+    
+    fig = plt.figure(figsize=(ratio, ratio))
+    # enable when generating just the one photo
+    # plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
     for i in range(predictions.shape[0]):
-        plt.subplot(1, 1, i+1)
+        plt.subplot(ratio, ratio, i+1)
         if IS_RGB:
             img_rgb = tf.clip_by_value(predictions[i] * 127.5 + 127.5, 0, 255)
             img_rgb = tf.cast(img_rgb, tf.uint8)
