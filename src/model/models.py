9611ffa6529b6ea765eaec7afa3b0e5fdf9df1d8
GMihai00 <mihai.gherghinscu15@gmail.com>
2024-08-31 16:25:48 +0300

    Started toying around with costume models

--- src/model/models.py
+++ src/model/models.py
@@ -2,6 +2,23 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Dropout,Conv2D, MaxPooling2D, Flatten, InputLayer
 
+def VGG16Modified(input_shape, output_shape):
+    base_model = tf.keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=input_shape)
+    for layer in base_model.layers:
+        layer.trainable = False
+        
+    x = base_model.output
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dense(1024, activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.5)(x) 
+    x = tf.keras.layers.Dense(512, activation='relu')(x) 
+    x = tf.keras.layers.BatchNormalization()(x) 
+    x = tf.keras.layers.Dense(256, activation='relu')(x) 
+    predictions = tf.keras.layers.Dense(output_shape, activation='softmax')(x) 
+        
+    return Model(inputs=base_model.input, outputs=predictions)
+    
+
 def VGG16(input_shape, output_shape):
     base_model = tf.keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=input_shape)
     for layer in base_model.layers:
